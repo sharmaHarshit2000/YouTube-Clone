08154fe833b114d34f53a5738d2b157a43b999b6
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-05-29 19:42:15 +0530

    error handling & not found middleware setup

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import connectDB from "./config/db.js";
 
-//Routes
-import authRoutes from "./routes/authRoutes.js"
-import channelRoutes from "./routes/channelRoutes.js"
-import videoRoutes from "./routes/videoRoutes.js"
+// Routes
+import authRoutes from "./routes/authRoutes.js";
+import channelRoutes from "./routes/channelRoutes.js";
+import videoRoutes from "./routes/videoRoutes.js";
+
+// Error middleware
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 dotenv.config();
 connectDB();
+
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//API Routes
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/channels", channelRoutes);
 app.use("/api/videos", videoRoutes);
 
-const PORT = process.env.PORT || 5000;
+// Error handling middlewares 
+app.use(notFound);
+app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
