09be932dd2a1a19237a0768a9358a63d551da32f
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-05-31 19:18:48 +0530

    setup the uiSlice

---
+++ frontend/src/features/ui/uiSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isSidebarOpen: true
+};
+
+const uiSlice = createSlice({
+    name: "ui",
+    initialState,
+    reducers: {
+        toggleSidebar: (state) => {
+            state.isSidebarOpen = !state.isSidebarOpen;
+        },
+
+        openSidebar: (state) => {
+            state.isSidebarOpen = true;
+        },
+        closeSidebar: (state) => {
+            state.isSidebarOpen = false;
+        }
+    }
+
+});
+
+export const {toggleSidebar, openSidebar, closeSidebar} = uiSlice.actions;
+export default uiSlice.reducer;
