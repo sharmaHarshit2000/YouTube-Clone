7a24f0d9f3c08e00897ec0fb17ab7b702656e4d7
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-06-01 09:19:04 +0530

    setup the layout

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -5,7 +5,10 @@ const AppRouter = () => {
 
     <Routes>
 
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:videoId" element={<VideoWatchPage />} />
+        <Route path="/channel/:id" element={<ChannelPage />} />
+
     </Routes>
 }
 
