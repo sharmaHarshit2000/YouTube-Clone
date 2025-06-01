59347397cce6d63c716f6f883c4cad115dd7474f
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-06-01 11:05:28 +0530

    toast and side bar setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,47 @@
-import AppRouter from "./router/AppRouter";
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import AppRouter from './router/AppRouter';
+import Layout from './components/Layout';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUser } from './features/auth/authSlice';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-    
-    return (
+export default function App() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-        <>
-        
-        <AppRouter/>
-        </>
-    )
+  const toggleSidebar = () => {
+    setSidebarOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token && !user) {
+      dispatch(fetchUser());
+    }
+  }, [dispatch, user]);
+
+  return (
+    <>
+      <Header toggleSidebar={toggleSidebar} />
+      <Sidebar isOpen={sidebarOpen} />
+      <Layout>
+        <AppRouter isAuthenticated={!!user} />
+      </Layout>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
 }
-
-
-export default App;
