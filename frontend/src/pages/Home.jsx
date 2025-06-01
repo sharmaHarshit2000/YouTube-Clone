9591c24f7d3740f528c4bc9ee6e656b35b747419
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-06-01 09:05:57 +0530

    setup the header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { fetchAllVideos } from "../features/video/videoSlice";
+
 const Home = () => {
-    
+    const dispatch = useDispatch();
+    const { videos, loading } = useSelector((state) => state.videos);
+    const { term } = useSelector((state) => state.search);
+    const { isSidebarOpen } = useSelector((state) => state.ui);
+
+    const [selectedFilter, setSelectedFilter] = useState("All");
+
+    useEffect(() => {
+        dispatch(fetchAllVideos());
+    }, [dispatch]);
+
+    const handleSelectedFilter = (filter) => {
+        if(selectedFilter === filter) {
+            setSelectedFilter("All")
+        } else {
+            setSelectedFilter(filter)
+        }
+    }
+
+    const filteredVideo = useMemo(() => {
+        let filtered
+    })
+
 }
 
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,17 @@
-import {configureStore} from "@reduxjs/toolkit";
-import authReducer from "./features/auth/authSlice"
-
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./features/auth/authSlice";
+import channelReducer from "./features/channel/channelSlice";
+import videoReducer from "./features/video/videoSlice";
+import commentReducer from "./features/comment/commetSlice";
+import searchReducer from "./features/search/searchSlice";
 export default configureStore({
-    reducer:{
-        auth: authReducer
-    }
-})
+  reducer: {
+    auth: authReducer,
+    channel: channelReducer,
+    videos: videoReducer,
+    comments: commentReducer,
+    search: searchReducer,
+  
+
+  },
+});
